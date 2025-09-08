9f8cfb8391d293d68fc22baba87ab234970cc2b3
simon0302010 <100756138+simon0302010@users.noreply.github.com>
2025-09-08 22:07:15 +0200

    added fonts and code to prepare to similarity checking

---
+++ koba/similarity.py
@@ -0,0 +1,18 @@
+def get_amount(amount):
+    chars = []
+    i = 32 # first printable ascii
+    while len(chars) < amount and i <= 0x10FFFF:
+        c = chr(i)
+        if c.isprintable() and c.strip():
+            chars.append(c)
+        i += 1
+    return chars
+
+def get_range(start, end):
+    chars = []
+    for i in range(start, end + 1):
+        char = chr(i)
+        if char.isprintable() and char.strip():
+            chars.append(char)
+            
+    return chars
