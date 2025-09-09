952909f6b73dc2d75f58ba3b619e37245c4f76c7
simon0302010 <100756138+simon0302010@users.noreply.github.com>
2025-09-09 12:08:09 +0200

    improved code for getting suitable fonts

--- koba/font.py
+++ koba/font.py
@@ -1,11 +1,38 @@
 import matplotlib.font_manager as fm
+from PIL import ImageFont
 
 
-def get_fonts():
-    font_paths = []
+PREFERRED = [
+    "DejaVuSansMono",
+    "LiberationMono",
+    "CourierNew",
+    "Menlo",
+    "JetBrainsMono",
+]
+
+def get_fonts(preferred=False):
+    fonts = []
+    seen_names = set()
     font_list = fm.fontManager.ttflist
 
     for font in font_list:
-        font_paths.append(font.fname)
+        if font.name in seen_names:
+            continue
+        if preferred:
+            if any(name.lower() in font.name.lower() for name in PREFERRED):
+                fonts.append(font)
+                seen_names.add(font.name)
+        else:
+            fonts.append(font)
+            seen_names.add(font.name)
 
-    return font_paths
+    return [font.fname for font in fonts]
+
+# very slow
+def is_monospace(path, test_chars="ilMW"):
+    font = ImageFont.truetype(path, 16)
+    widths = [font.getsize(c)[0] for c in test_chars]
+    return all(w == widths[0] for w in widths)
+
+if __name__ == "__main__":
+    print(get_fonts(preferred=True))
