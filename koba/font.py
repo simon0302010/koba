b196bec9bea3bbc1ca349fb67c4f76d723ee0337
simon0302010 <100756138+simon0302010@users.noreply.github.com>
2025-09-09 13:10:25 +0200

    added code to get the best font out of a list

--- koba/font.py
+++ koba/font.py
@@ -8,9 +8,11 @@ PREFERRED = [
     "CourierNew",
     "Menlo",
     "JetBrainsMono",
+    "Consolas",
+    "Cascadia"
 ]
 
-def get_fonts(preferred=False):
+def get_fonts(preferred=False, monospace=False):
     fonts = []
     seen_names = set()
     font_list = fm.fontManager.ttflist
@@ -28,6 +30,36 @@ def get_fonts(preferred=False):
 
     return [font.fname for font in fonts]
 
+def get_best_candidate(font_paths):
+    style_rank = {
+        "Regular": 0,
+        "Book": 1,
+        "Normal": 2,
+        "Medium": 3,
+        "SemiBold": 4,
+        "Bold": 5,
+        "ExtraBold": 6,
+        "Light": 7,
+        "ExtraLight": 8,
+        "Thin": 9,
+    }
+    
+    # only monospace fonts that aren't italic
+    candidates = [
+        p for p in font_paths
+        if "mono" in p.lower() and "italic" not in p.lower() and "propo" not in p.lower()
+    ]
+    
+    # rank fonts
+    def score(path):
+        for style, rank in style_rank.items():
+            if style.lower() in path.lower():
+                return rank
+        return 99
+
+    best = min(candidates, key=score, default=None)
+    return best
+
 # very slow
 def is_monospace(path, test_chars="ilMW"):
     font = ImageFont.truetype(path, 16)
@@ -35,4 +67,5 @@ def is_monospace(path, test_chars="ilMW"):
     return all(w == widths[0] for w in widths)
 
 if __name__ == "__main__":
-    print(get_fonts(preferred=True))
+    font_paths = get_fonts(preferred=True)
+    print(get_best_candidate(font_paths))
